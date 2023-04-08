f55da4fcba52299c6cec1522dd453123c7064681
verma0031 <peeyushverma888@gmial.com>
2023-04-08 20:10:05 +0530

    added cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -33,6 +34,17 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+
+  Product.findById(prodId, (product) =>{
+    Cart.addProduct(prodId, product.price)
+  });
+
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
