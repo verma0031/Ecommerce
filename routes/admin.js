a1f2793c0f9caeb9a2d0b1408982ad35e055d649
verma0031 <peeyushverma888@gmial.com>
2023-04-09 23:15:24 +0530

    added edit and delete products functionality

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,14 @@ router.get('/add-product', adminController.getAddProduct);
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId' , adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProducts);
+
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.post('/delete-product', adminController.postDeleteProducts);
 
 module.exports = router;
